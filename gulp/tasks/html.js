28ee53f9c5bb5118de3496aa62b9be571a2c9ce9
Mon Lu <mon@ave81.com>
2015-02-20 11:22:48 -0600

    Removed minify html

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp-help')(require('gulp'));
-var minify = require('gulp-minify-html');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
 
-gulp.task('html', 'Minify index.html and LiveReload on the browser', function () {
+gulp.task('html', 'Watch index.html with LiveReload', function () {
   var opts = {comments:true,spare:true};
 
   gulp.src('./leadpages-template/index.html')
