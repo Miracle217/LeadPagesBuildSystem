5f1b567b0de5e69b99ff177d6d6700af77a307d0
Mon Lu <mon@ave81.com>
2015-04-16 10:45:33 -0500

    Wrong src path for copying

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp-help')(require('gulp'));
 
-var path = './build/dist/leadpages-template/**/*';
-var destination = './build/dist/leadpages-template';
+var path = {
+	src: './leadpages-template/**/*',
+	dest: './build/dist/leadpages-template'
+};
 
 gulp.task('copy', 'Make a temp copy of leadpages-template for zipping', function(cb){
-	gulp.src(path)
-		.pipe(gulp.dest(destination));
+	gulp.src(path.src)
+		.pipe(gulp.dest(path.dest));
 	cb();
 });
